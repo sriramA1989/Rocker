1917bd800f76b9a978fb4058cd453fc8a7b5413f
sriramA1989 <sriram.jaihind123@gmail.com>
2019-11-24 20:21:36 +0530

    Codes Added

---
+++ src/org/acn/Hello.java
@@ -0,0 +1,13 @@
+package org.acn;
+
+public class Hello {
+
+	public void time() {
+		System.out.println("Hello World");
+	}
+	public static void main(String[] args) {
+	Hello k = new Hello();
+	k.time();
+	
+	}
+}
