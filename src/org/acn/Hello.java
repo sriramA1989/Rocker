2cb02e2413ca5957a103b5134f88335b53b92645
sriramA1989 <sriram.jaihind123@gmail.com>
2019-11-24 21:00:23 +0530

    msg

--- src/org/acn/Hello.java
+++ src/org/acn/Hello.java
@@ -4,10 +4,18 @@ public class Hello {
 
 	public void time() {
 		System.out.println("Hello World");
+		
 	}
+	public void seconds() {
+		System.out.println("Hi Seconds");
+
+	}
+	
 	public static void main(String[] args) {
 	Hello k = new Hello();
 	k.time();
+	k.seconds();
+
 	
 	}
 }
