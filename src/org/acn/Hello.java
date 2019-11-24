27a7ae4be14adf247b5c75f985441931bd6afd15
sriramA1989 <sriram.jaihind123@gmail.com>
2019-11-24 20:33:27 +0530

    code

--- src/org/acn/Hello.java
+++ src/org/acn/Hello.java
@@ -5,9 +5,14 @@ public class Hello {
 	public void time() {
 		System.out.println("Hello World");
 	}
+	public void space() {
+	System.out.println("Hello Space");
+
+	}
 	public static void main(String[] args) {
 	Hello k = new Hello();
-	k.time();
 	
+	k.time();
+	k.space();
 	}
 }
